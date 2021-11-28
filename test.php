69046f9d054f36784e4f81b3127814f390349e24
lumiwazeare <lumiwazeare@yahoo.com>
2021-11-28 18:16:22 +0100

    version

--- test.php
+++ test.php
@@ -3,8 +3,6 @@ require_once "vendor/autoload.php";
 
 use stacklogio\StackLog;
 
-$log = new StackLog("n9185kn4h542h7ihyttez24maykbzj","7aibvmlanrfppxzowwbwx5qp2zgzr1");
-//throw new Exception("not found in except");
+$log = new StackLog("sk_jli9t5rqlcd8qzvox4fffzd8luri66","bk_dx8xonr4m1rnbqub4moj2ol7ldwmnt");
+print_r($log->info("testing from php"));
 
-$data = ["name" => ""];
-echo  $data["friend"];
